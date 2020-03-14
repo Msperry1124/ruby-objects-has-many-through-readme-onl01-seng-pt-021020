aa5294baf8894cc0d1df21e34f1bd9ae26ca28c7
Melissa Sperry <sperry.mel@gmail.com>
2020-03-14 14:15:23 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,25 +1,25 @@
 class Customer
-  attr_accessor :name, :age 
+  attr_accessor :name, :age
   @@all = []
-  def initialize (name, age) 
+  def initialize (name, age)
     @name = name
-    @age = age 
-    @@all << self 
-  end 
+    @age = age
+    @@all << self
+  end
   def self.all
-    @@all 
-  end 
-  def new_meal(waiter, total, tip = 0) 
-    Meal.new(waiter, self, total, tip) 
-  end 
+    @@all
+  end
+  def new_meal(waiter, total, tip = 0)
+    Meal.new(waiter, self, total, tip)
+  end
   def meals
     Meal.all.select do |meal|
-      meal.customer == self 
-    end 
-  end 
-  def waiters 
+      meal.customer == self
+    end
+  end
+  def waiters
     meals.map do |meal|
-      meal.waiter 
-    end 
-  end 
+      meal.waiter
+    end
+  end
 end
